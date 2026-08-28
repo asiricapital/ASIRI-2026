66f5efa602de0cc65f0126883fbbc9096a4d7440
asiricapital <aalaseeri.a@gmail.com>
2026-08-28 21:11:25 +0000

    Load YouTube hub from Music OS shell

--- asiri-music-staging/src/os-shell.js
+++ asiri-music-staging/src/os-shell.js
@@ -1,5 +1,6 @@
 import {QUICK_MOMENTS,momentForHour,personalizeMomentPrompt} from './moment-core.js?v=20260808-moment-v1';
 import './home-intelligence.js?v=20260828-v1';
+import './youtube-hub.js?v=20260829-youtube-v1';
 
 const $=s=>document.querySelector(s);
 const $$=s=>[...document.querySelectorAll(s)];
